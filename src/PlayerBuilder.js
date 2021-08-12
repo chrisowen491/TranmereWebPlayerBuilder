42b912a0d03d2ee43af7c230dd5d34da0d73f1c2
chrisowen <chrisowen491@gmail.com>
2021-08-12 13:49:33 +0100

    change caching

--- src/PlayerBuilder.js
+++ src/PlayerBuilder.js
@@ -25,7 +25,7 @@ exports.handler = async function (event, context) {
         "headers": { 
             "Content-Type": "image/svg+xml", 
             "Access-Control-Allow-Origin": "*",
-            "Cache-Control": "Cache-Control: public"
+            "Cache-Control": "Cache-Control: public, max-age=2592000"
         },
         "statusCode": 200,
         "body": svg
